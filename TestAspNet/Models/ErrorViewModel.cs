2d7a8988f658f8901f5918f5391fbb6d0369e8f2
hawaktor@gmail.com <maksymsyzonenkocorporate@gmail.com>
2024-06-22 15:21:19 +0300

    Initial

---
+++ TestAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestAspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
